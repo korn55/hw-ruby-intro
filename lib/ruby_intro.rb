b4ee46186ed8cfb3815432d2c622717a67463f8c
korn55 <6620301001@cdti.ac.th>
2024-12-19 07:05:39 +0000

    begin1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,46 @@
 
 # Part 1
 
+# Define a method to sum all elements in an array
 def sum(arr)
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+ 
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  
 end
 
 # Part 2
+def hello(name)
+  
+end
+
+def starts_with_consonant? s
+ 
+end
+
+def binary_multiple_of_4? s
+  
+end
 
 def hello(name)
-  # YOUR CODE HERE
+ 
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  
 end
